a318d72c7b7d8c0091713b2837cbf516dff925ca
HassainAhmed <124057658+HassainAhmed@users.noreply.github.com>
2023-02-05 00:11:46 -0800

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Alert = () => {
+    return (
+        <div>
+            <div className="alert alert-primary" role="alert">
+                {"Hassain One"}
+            </div>
+        </div>
+    )
+}
---
+++ App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />}/>
+              <Route exact path="/about" element={<About></About>}/>
+              <Route exact path="/login" element={<Login />}/>
+              <Route exact path="/signup"element={ <Signup />}/>
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
+    </>
+  );
+}
+
+export default App;
